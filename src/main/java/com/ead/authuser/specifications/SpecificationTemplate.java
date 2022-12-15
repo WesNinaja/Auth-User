bf6a536d8ef3034644a6eb08d2298281b5768df2
wesley.calle <wesley.calle@idwall.co>
2022-12-14 22:24:12 -0300

    feat: API Composition Pattern in Authuser Microservice and adding client to request course with RestTemplate

--- src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.specifications;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
@@ -7,6 +8,9 @@ import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Join;
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -19,4 +23,15 @@ public class SpecificationTemplate {
             @Spec(path = "fullName", spec = LikeIgnoreCase.class)
     })
     public interface UserSpec extends Specification<UserModel> {}
+
+    public static Specification<UserModel> userCourseId(final UUID courseId){
+        return (root, query, criteriaBuilder) -> {
+
+            query.distinct(true); //Aqui defino que vou querer apenas dados que não se repetem
+            Join<UserModel, UserCourseModel> userProd = root.join("usersCourses"); //Nesta linha faço um simples join, passando as duas entidades e qual relação entre elas
+            //EX: SELECT * ALL TB_USERS
+            //JOIN TB_USERS_COURSES ON COURSE_ID = COURSE_ID
+            return criteriaBuilder.equal(userProd.get("courseId"), courseId);
+         };
+    }
 }
